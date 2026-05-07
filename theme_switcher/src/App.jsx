45e35d84254c5758dc78724e1847d08dfa27445e
zaifpasha23422 <zaifpasha23422@gmail.com>
2026-05-07 18:04:52 +0530

    todo

--- theme_switcher/src/App.jsx
+++ theme_switcher/src/App.jsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 import ThemeBtn from "./components/ThemeBtn";
 import Card from "./components/Card";
 
+
 function App() {
   const [themeMode, setThemeMode] = useState("light");
 
@@ -27,7 +28,6 @@ function App() {
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
             <ThemeBtn />
           </div>
-
           <div className="w-full max-w-sm mx-auto">
             <Card />
           </div>
